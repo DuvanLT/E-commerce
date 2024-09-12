320fc0e7e8d8413936c5433f9ea94341871fce9b
DuvanLT <duvanred0@gmail.com>
2024-09-12 16:50:59 -0500

    traida y muestra de datos

---
+++ src/app/api/Api.jsx
@@ -0,0 +1,24 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+function useFetch(url) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                console.log(error)
+            } 
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data};
+}
+
+export default useFetch;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,15 @@
+'use client'
+import useFetch from './api/Api';
+import Card from './component/Card'
 export default function Home() {
-  return (
-    <>here start an amazing project</>
+ 
+    const { data } = useFetch('https://fakestoreapi.com/products?limit=20');
+
+    return (
+        <div className='container'>
+            {data.map(item => (
+             <Card key={item.id} title={item.title} image={item.image} price={item.price}  />
+            ))}
+        </div>
   );
 }
